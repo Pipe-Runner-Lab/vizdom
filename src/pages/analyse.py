7095c12c9911a9ce1d70149d305a9bbc7b57a3a7
Pipe-Runner <piperunner.in@gmail.com>
2022-12-05 20:54:15 +0100

    Main graph swap added

--- src/pages/analyse.py
+++ src/pages/analyse.py
@@ -17,18 +17,35 @@ from components.filter_input.filter_input import render_filter_input
 
 countries = get_list_of_countries()
 list_of_attributes = get_our_world_in_data_attributes.items()
-list_of_real_attributes = get_our_world_in_data_real_attributes.items() 
+list_of_real_attributes = get_our_world_in_data_real_attributes.items()
 # * Register route
 register_page(__name__, path="/analyse")
 
 layout = three_splitter_v2(
     main_1=[
-        html.Div(dcc.Loading(
-            dcc.Graph(figure={},
-                      id="analyse-main-graph-1",
-                      className="main-graph",
-                      style={"opacity": "0"})),
-                 className="card"),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        dcc.Loading(
+                            dcc.Graph(figure={},
+                                      id="analyse-main-graph-1",
+                                      className="main-graph",
+                                      style={"opacity": "0"})
+                        )
+                    ],
+                    className="card"
+                ),
+                html.Button(
+                    html.I(className="fa-sharp fa-solid fa-exchange"),
+                    id='analyse-swap-button',
+                    className="swap-button",
+                    n_clicks=0
+                ),
+                dcc.Store(id='analyse-main-graph-swap', data=False),
+            ],
+            className="swap-container"
+        ),
         html.Div(
             dcc.Loading(dash_table.DataTable(id="correlation-table")),
             className="table-container card",
@@ -193,6 +210,19 @@ layout = three_splitter_v2(
 # ---------------------------------------------------------------------------- #
 
 
+@callback(
+    Output("analyse-main-graph-swap", "data"),
+    Input("analyse-main-graph-swap", "data"),
+    Input("analyse-swap-button", "n_clicks"),
+    prevent_initial_call=True
+)
+def toggle_main_graph(data, n_clicks):
+    if ctx.triggered_id == 'analyse-swap-button':
+        return not data
+    else:
+        raise PreventUpdate
+
+
 @callback(Output("analyse-filter-panel", "style"),
           Input("analyse-country-dropdown", "value"))
 def toggle_filter_panel(iso_code):
@@ -324,18 +354,19 @@ def aggregation_logic_enforcer(aggregation_type_1, aggregation_type_2):
     Input("analyse-aggregation2-dropdown", "value"),
     Input("analyse-bottom-graph", "relayoutData"),
     Input("analyse-filter-data", "data"),
+    Input("analyse-main-graph-swap", "data")
 )
-def update_all_graphs(iso_code, attribute_1, attribute_2, aggregation_type_1, aggregation_type_2, relayoutData, filter_data):
+def update_all_graphs(iso_code, attribute_1, attribute_2, aggregation_type_1, aggregation_type_2, relayoutData, filter_data, should_swap):
     if aggregation_type_1 == 'none' and aggregation_type_2 != 'none':
         raise PreventUpdate
     elif aggregation_type_1 != 'none' and aggregation_type_2 == 'none':
         raise PreventUpdate
-    
+
     start_date, end_date = get_date_range(relayoutData)
     column_data = None
     columns = None
     style = [{}]
-    
+
     if iso_code == "All":
         # country filter only checked if ISO Code is All
         filter_data = json.loads(filter_data)
@@ -346,24 +377,26 @@ def update_all_graphs(iso_code, attribute_1, attribute_2, aggregation_type_1, ag
         attribute_date_2 = get_attribute(attribute_2, start_date, end_date,
                                          iso_code, aggregation_type_2)
         attribute_date_1[attribute_2] = attribute_date_2[attribute_2]
-        
+
         column_data, columns, style = create_table_bar_styles_multiple_countries(
             attribute_1, attribute_2, start_date, end_date, iso_code)
-        
+
         if aggregation_type_1 != 'none' and aggregation_type_2 != 'none':
             fig2 = render_bar_compare(attribute_date_1, attribute_1,
                                       attribute_2, "location")
-            fig1 = render_scatter(attribute_date_1, attribute_1, attribute_2, 'location', aggregation_type_1, aggregation_type_2)
+            fig1 = render_scatter(attribute_date_1, attribute_1, attribute_2,
+                                  'location', aggregation_type_1, aggregation_type_2)
         else:
             attribute_date_mean_1 = get_attribute(attribute_1, start_date, end_date,
-                                         iso_code, 'mean')
+                                                  iso_code, 'mean')
             attribute_date_mean_2 = get_attribute(attribute_2, start_date, end_date,
-                                         iso_code, 'mean')
+                                                  iso_code, 'mean')
             attribute_date_mean_1[attribute_2] = attribute_date_mean_2[attribute_2]
             fig2 = render_country_lines(attribute_date_1,
                                         attribute_1, attribute_2, "date",
                                         "location")
-            fig1 = render_scatter(attribute_date_mean_1, attribute_1, attribute_2, 'location', 'mean', 'mean')
+            fig1 = render_scatter(
+                attribute_date_mean_1, attribute_1, attribute_2, 'location', 'mean', 'mean')
             fig1.update_layout(
                 margin=dict(r=12, t=24, b=16),
             )
@@ -376,7 +409,7 @@ def update_all_graphs(iso_code, attribute_1, attribute_2, aggregation_type_1, ag
         column_data, columns, style = create_table_bar_styles(
             attribute_1, start_date, end_date, iso_code)
         if aggregation_type_1 != 'none' and aggregation_type_2 != 'none':
-            
+
             fig1 = render_scatter(attribute_data_1,
                                   attribute_1, attribute_2, None, aggregation_type_1, aggregation_type_2)
             fig2 = render_bar_compare(attribute_data_1, attribute_1,
@@ -387,6 +420,10 @@ def update_all_graphs(iso_code, attribute_1, attribute_2, aggregation_type_1, ag
                                   attribute_1, attribute_2)
             fig2 = render_two_lines(attribute_data_1,
                                     attribute_1, attribute_2, "date")
+
+    if should_swap:
+        fig1, fig2 = fig2, fig1
+        
     return fig1, fig2, {
         "opacity": "1"
     }, {
