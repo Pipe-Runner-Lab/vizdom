8c4f4f1051b327931aeae4ba9395c977a3a74ac7
Andreas <abullenger88@gmail.com>
2022-12-07 00:50:10 +0100

    adding grouping to explore or in the middle of

--- src/pages/analyse.py
+++ src/pages/analyse.py
@@ -395,7 +395,6 @@ def update_filter(n_clicks, filter_type, countries, attribute, filter_expression
 
         countries, group_data = get_simple_filtered_countries(continents, group=group, selected_group=selected_group, should_group=should_group)
 
-        print(group_data)
 
         success_message = "Found " + str(len(countries)) + " countries" if len(
             countries) > 0 else "No countries found, showing all countries"
